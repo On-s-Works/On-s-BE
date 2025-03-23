b8acd99d11961a41a57cefefb842e8c8f0da9a98
wnstj1548 <wnstj0614@naver.com>
2025-03-23 20:13:00 +0900

    feat : "cors 설정 추가"

---
+++ src/main/java/com/ons/back/commons/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ons.back.commons.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://on-s-fe.vercel.app", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

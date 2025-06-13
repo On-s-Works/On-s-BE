76781f0050121fdb53aedfea8a0499e457ca3c20
wnstj1548 <wnstj0614@naver.com>
2025-06-13 21:34:20 +0900

    feat : "OAuth2 인증 성공 시 프론트로 리다이렉트하게 변경"

--- src/main/java/com/ons/back/commons/handler/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/ons/back/commons/handler/CustomOAuth2SuccessHandler.java
@@ -5,11 +5,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Component
@@ -18,7 +22,7 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
     private final TokenProvider tokenProvider;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         String accessToken = tokenProvider.generateAccessToken(authentication);
         String refreshToken = tokenProvider.generateRefreshToken(authentication, accessToken);
@@ -26,9 +30,20 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        String jsonResponse = "{\"accessToken\": \"" + accessToken + "\", \"refreshToken\": \"" + refreshToken + "\"}";
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refresh_token", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/")
+                .maxAge(Duration.ofDays(1))
+                .build();
 
-        response.getWriter().write(jsonResponse);
-        response.getWriter().flush();
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+
+        String redirectUri = UriComponentsBuilder.fromUriString("https://on-s-fe.vercel.app/auth/callback")
+                .queryParam("accessToken", accessToken)
+                .build().toUriString();
+
+        response.sendRedirect(redirectUri);
     }
 }

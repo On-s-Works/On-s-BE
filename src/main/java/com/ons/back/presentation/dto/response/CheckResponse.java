287bfde7b44eeaf7e8eac636d0b93a4fd5957a91
wnstj1548 <wnstj0614@naver.com>
2025-08-27 21:08:11 +0900

    feat : "validation 추가"

--- src/main/java/com/ons/back/presentation/dto/response/CheckResponse.java
+++ src/main/java/com/ons/back/presentation/dto/response/CheckResponse.java
@@ -1,6 +1,7 @@
 package com.ons.back.presentation.dto.response;
 
 import com.ons.back.persistence.domain.User;
+import com.ons.back.persistence.domain.type.ProviderType;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -8,12 +9,14 @@ import java.time.LocalDateTime;
 @Builder
 public record CheckResponse(
         String uid,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        ProviderType providerType
 ) {
     public static CheckResponse fromUser(User user) {
         return CheckResponse.builder()
                 .uid(user.getUid())
                 .createdAt(user.getCreatedAt())
+                .providerType(user.getProviderType())
                 .build();
     }
 }
